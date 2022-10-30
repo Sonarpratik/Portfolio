b12bf0bfae16820d9965f56de2f6e2c2ad3625fc
Sonarpratik <sonarpratik02.ps@gmail.com>
2022-10-30 12:12:22 +0530

    shch

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
               data-aos-delay="1000"
               data-aos-duration="1000"
             >
-              Sonar Pratik
+              Sonar Sonu
             </h1>
             <p
               className="for-size"
